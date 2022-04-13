0eaf61d0315b336229e703f96473cd76924fd265
Nissa McKinney <nissa2424@gmail.com>
2022-04-12 20:09:57 -0500

    Controller

--- SpinRecycle/Controllers/CartController.cs
+++ SpinRecycle/Controllers/CartController.cs
@@ -7,5 +7,36 @@ namespace SpinRecycle.Controllers
     [ApiController]
     public class CartController : ControllerBase
     {
+        RecordRepository _recRepo = new RecordRepository();
+
+        [HttpPost]
+        public IActionResult Post(Cart newCart)
+        {
+            if (!ValidNewCart(newRecord / Cart))
+            {
+                return BadRequest(newRecord / Cart);
+            }
+            else
+            {
+                _recRepo.Post(newRecord / Cart);
+                return Ok(newRecord / Cart);
+            }
+
+        }
+        [HttpDelete("cart/{cart}")]
+        public IActionResult DeleteCart(string cart)
+        {
+            var matchingCart = _recRepo.GetByName(cart);
+            if (matchingCart == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _recRepo.DeleteCart((cart)matchingCart);
+                return NoContent();
+            }
+        }
     }
 }
+
--- SpinRecycle/Controllers/RecordController.cs
+++ SpinRecycle/Controllers/RecordController.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
+using SpinRecycle.Data_Access;
+using SpinRecycle.Models;
 
 namespace SpinRecycle.Controllers
 {
@@ -7,5 +9,72 @@ namespace SpinRecycle.Controllers
     [ApiController]
     public class RecordController : ControllerBase
     {
+        //RecordRepository _recRepo = new RecordRepository();
+
+        [HttpGet] //Method
+        public List<Record> GetAllRecords()
+        {
+            return _recRepo.GetAll();
+        }
+
+        [HttpGet("id/{id}")]
+        public IActionResult GetRecordById(string id)
+        {
+            var match = _recRepo.GetRecordById(id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(match);
+        }
+
+        [HttpGet("name/{name}")]
+        public IActionResult GetRecordByName(string name)
+        {
+            var match = _recRepo.GetRecordByName(name);
+            if (match == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(match);
+        }
+        [HttpGet("artist/{artist}")]
+        public IActionResult GetRecordByArtist(string artist)
+        {
+            var match = _recRepo.GetByArtist(artist);
+            if (match == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(match);
+        }
+        [HttpGet("genre/{genre}")]
+        public IActionResult GetRecordByGenre(string genre)
+        {
+            var match = _recRepo.GetByGenre(genre);
+            if (match == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(match);
+        }
+        [HttpGet("searching/{searching}")]
+        public IActionResult GetRecordBySearching(string searching)
+        {
+            var match = _recRepo.GetBySearching(searching);
+            if (match == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(match);
+        }
+
+
     }
 }
+
