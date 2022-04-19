3aa54984b8cd4c8c6d3ef4632832f36d2470f6c0
Craig <wellspring.craig@gmail.com>
2022-04-18 20:55:20 -0500

    CartRecordJoin

--- SpinRecycle/Data Access/ICartRepository.cs
+++ SpinRecycle/Data Access/ICartRepository.cs
@@ -1,6 +1,17 @@
-﻿namespace SpinRecycle.Data_Access
+﻿using SpinRecycle.Models;
+
+namespace SpinRecycle.Data_Access
 {
     public interface ICartRepository
     {
+        // Carts
+        Cart CreateNewCart();
+        void DeleteCart(int cartId);
+
+        // Cart-Record Joins
+        List<Record> GetCartRecords(int cartId);
+        Record AddToCart(int recordId, int cartId);
+
+        void RemoveFromCart(int recordId, int cartId);
     }
 }
---
+++ SpinRecycle/Models/CartRecordJoin.cs
@@ -0,0 +1,9 @@
+﻿namespace SpinRecycle.Models
+{
+    public class CartRecordJoin
+    {
+        public int Id { get; set; }
+        public int CartId { get; set; }
+        public int RecordId { get; set; }
+    }
+}
