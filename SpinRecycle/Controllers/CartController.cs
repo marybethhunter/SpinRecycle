8b1e65e46c4fff411d6da34cb638b778ec299743
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-04-19 19:12:36 -0500

    created cart controller with methods and updated icartrepository to match

---
+++ SpinRecycle/Controllers/CartController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpinRecycle.Data_Access;
+using SpinRecycle.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpinRecycle.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+        private readonly ICartRepository _cartRepo;
+        public CartController(ICartRepository cartRepository)
+        {
+            _cartRepo = cartRepository;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCartById(int id)
+        {
+            var match = _cartRepo.GetCartById(id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+            return Ok(match);
+        }
+
+        [HttpPost]
+        public IActionResult Post(Cart newCart)
+        {
+            _cartRepo.AddCart(newCart);
+            return Ok(newCart);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, Cart cartToUpdate)
+        {
+            var match = _cartRepo.GetCartById(id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+            _cartRepo.UpdateCart(id, cartToUpdate);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var match = _cartRepo.GetCartById(id);
+            if (match == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                _cartRepo.DeleteCart(id);
+                return NoContent();
+            }
+        }
+    }
+}
--- SpinRecycle/Data Access/ICartRepository.cs
+++ SpinRecycle/Data Access/ICartRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace SpinRecycle.Data_Access
+﻿using SpinRecycle.Models;
+
+namespace SpinRecycle.Data_Access
 {
     public interface ICartRepository
     {
+        Cart GetCartById(int id);
+        void AddCart(Cart cart);
+        void UpdateCart(int id, Cart cart);
+        void DeleteCart(int id);
     }
 }
