18eca5b9f7bc524b604538825127c54b13a35ae0
Craig <wellspring.craig@gmail.com>
2022-04-22 21:39:42 -0500

    Initial setup

--- SpinRecycle/Data Access/ICartRepository.cs
+++ SpinRecycle/Data Access/ICartRepository.cs
@@ -5,18 +5,18 @@ namespace SpinRecycle.Data_Access
     public interface ICartRepository
     {
         // Carts
-        Cart CreateNewCart();
-        void DeleteCart(int cartId);
+        // Cart CreateNewCart();
+        // void DeleteCart(int cartId);
 
         // Cart-Record Joins
-        List<Record> GetCartRecords(int cartId);
-        Record AddToCart(int recordId, int cartId);
+        // List<Record> GetCartRecords(int cartId);
+        // Record AddToCart(int recordId, int cartId);
 
-        void RemoveFromCart(int recordId, int cartId);
+        // void RemoveFromCart(int recordId, int cartId);
       
-        //Cart GetCartById(int id);
-        //void AddCart(Cart cart);
-        //void UpdateCart(int id, Cart cart);
-        //void DeleteCart(int id);
+        Cart GetCartById(int id);
+        void AddCart(Cart cart);
+        void UpdateCart(int id, Cart cart);
+        void DeleteCart(int id);
     }
 }
--- spinrecycleclient/src/components/Nav.js
+++ spinrecycleclient/src/components/Nav.js
@@ -1,1 +1,7 @@
-console.log("Hello World!")
+import React from 'react'
+
+export default function Nav() {
+  return (
+    <div>Navbar</div>
+  )
+}
