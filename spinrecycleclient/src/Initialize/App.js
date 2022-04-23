2edc418b018ec27d44225ca4f1a22809236349b7
Craig Wellspring <61626143+Craig-Wellspring@users.noreply.github.com>
2022-04-22 23:53:35 -0500

    html skeleton

--- spinrecycleclient/src/Initialize/App.js
+++ spinrecycleclient/src/Initialize/App.js
@@ -1,11 +1,29 @@
 import Nav from '../components/Nav';
-import '../styles/App.css';
+import styled from 'styled-components';
+
+const Body = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+
+  background-color: pink;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+
+  width: 100%;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-    </div>
+    <Body>
+      <Nav className="nav-expanded" />
+      <Content>Content</Content>
+    </Body>
   );
 }
 
--- spinrecycleclient/src/components/Nav.js
+++ spinrecycleclient/src/components/Nav.js
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from 'react';
+import styled from 'styled-components';
+
+const BarBody = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 250px;
+
+  background-color: lightblue;
+`;
 
 export default function Nav() {
   return (
-    <div>Navbar</div>
+    <BarBody>Navbar</BarBody>
   )
 }
