25b2be3dac641fae5e4cf68019c0380cfa126da8
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-04-25 20:30:39 -0500

    created record card component, roughed in routes, and shop view

--- spinrecycleclient/src/views/Shop.js
+++ spinrecycleclient/src/views/Shop.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+import React, { useState, useEffect } from "react";
+import Record from "../components/Record";
+import getAllRecords from "../data/recordData";
+
+export default function Shop() {
+  const [records, setRecords] = useState([]);
+
+  useEffect(() => {
+    let isMounted = true;
+    getAllRecords().then((recordsArray) => {
+      if (isMounted) setRecords(recordsArray);
+    });
+  });
+
+  return (
+    <div>
+      {records.map((record) => (
+        <Record key={record.Id} record={record} />
+      ))}
+    </div>
+  );
+}
