7442d971e58e8e446ae4f2faf4c82d2852a41951
Craig <wellspring.craig@gmail.com>
2022-04-25 19:13:20 -0500

    record card

---
+++ spinrecycleclient/src/components/RecordCard.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const CardBody = styled.div`
+
+`;
+
+const AlbumCover = styled.img``;
+const AlbumInfo = styled.div`
+  display: flex;
+  flex-dire
+`;
+
+const Title = styled.div``;
+
+function RecordCard(props) {
+  return (
+    <CardBody>
+      <AlbumCover />
+
+      Record
+    </CardBody>
+  )
+}
+
+Record.propTypes = {}
+
+export default Record
