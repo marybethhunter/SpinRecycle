4ec8d5f91d31e9d07b33450ead480b091f0d5a5a
Nissa McKinney <nissa2424@gmail.com>
2022-04-26 20:19:30 -0500

    working backend

--- SpinRecycle/Data Access/CartRepository.cs
+++ SpinRecycle/Data Access/CartRepository.cs
@@ -5,47 +5,114 @@ namespace SpinRecycle.Data_Access
 {
     public class CartRepository : ICartRepository
     {
-        private static List<Cart> _carts = new List<Cart>()
+        private readonly IConfiguration _config;
+        public CartRepository(IConfiguration config) //constructor
         {
-            new Cart()
-            {
-                Id = 1,
-                CartId = "Prince",
-                RecordId = "1"
-                
-            },
-            new Cart()
-            {
-                Id = 2,
-                CartId = "Ari Lennox",
-                RecordId = "2"
-                
-            },
-            new Cart()
-            {
-                Id = 3,
-                CartId = "Luck Daye",
-                RecordId = "3"
-                
-            },
-            new Cart()
-            {
-                Id = 4,
-                CartId = "J Cole",
-                RecordId = "4"
-
-            },
-        };
-
-        internal object GetByCartId(string cart)
-        {
-            var match = _carts.FirstOrDefault(x => x.Cart == cart);
-            return match;
+            _config = config;
         }
 
-        internal List<Cart> GetAll()
+        public SqlConnection Connection
         {
-            return _carts;
+            get
+            {
+                return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            }
+        }
+
+        public List<Record> GetCartRecords()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT c.RecordId AS CartRecordId, 
+                                      r.RecordId AS RecordId,
+                                      a.[Name] AS ArtistName,
+                                      g.[Name] AS Genre,
+                                      r.Title, 
+                                      Price
+                                      FROM Cart As c
+                                      LEFT JOIN Record r 
+                                      ON c.RecordId = r.RecordId 
+                                      LEFT JOIN Artist as a
+                                      ON r.ArtistId = a.ArtistId
+                                      LEFT JOIN Genre as g
+                                      ON r.GenreId = g.GenreId
+                                             ";
+
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    List<Record> records = new List<Record>();
+                    while (reader.Read())
+                    {
+                        Record record = new Record
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("RecordId")),
+                            // Title
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            // Artist
+                            Artist = reader.GetString(reader.GetOrdinal("ArtistName")),
+                            // Genre
+                            Genre = reader.GetString(reader.GetOrdinal("GenreName")),
+                            // Price 
+                            Price = reader.GetDecimal(reader.GetOrdinal("Price")),
+                        };
+
+                        records.Add(record);
+
+
+                    }
+
+                    reader.Close();
+                    return records;
+                }
+            }
+        }
+
+
+
+        public void AddCartRecord(Record record)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO Cart 
+                    VALUES @RecordId;
+                     ";
+
+                    cmd.Parameters.AddWithValue("@RecordId", record.Id);
+                                  
+                }
+            }
+        }
+
+        public void DeleteCartRecord(int RecordId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            DELETE FROM Cart
+                            WHERE RecordId = @id
+                        ";
+
+                    cmd.Parameters.AddWithValue("@id", RecordId);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
+
+        
+
+  
+
