64996aa3d3219e5ceaecfc57d1d571ad4f4ceaf6
Mary Beth Hunter <mary.beth.hunter@icloud.com>
2022-04-26 21:16:01 -0500

    fixing package.json eslint thing

--- spinrecycleclient/src/routes/index.js
+++ spinrecycleclient/src/routes/index.js
@@ -3,13 +3,14 @@ import {
   Route,
   Routes,
 } from "react-router-dom";
+import Shop from "../views/Shop";
 
 export default function Routing() {
   return (
     <Routes>
       <Route exact path="" component />
       <Route exact path="" component />
-      <Route exact path="" component />
+      <Route exact path="/shop" component={() => <Shop />} />
     </Routes>
   );
 }
