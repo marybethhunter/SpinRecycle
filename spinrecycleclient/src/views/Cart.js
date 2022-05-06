09d9a1e6925f048975caf089d00d59b08049189b
Luke Allen <lukus2013@yahoo.com>
2022-05-05 20:22:34 -0500

    new

---
+++ spinrecycleclient/src/views/Cart.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import getCart from '../data/cartData';
+
+export default function Cart() {
+    const [cart, setCart] = useState([])
+
+    useEffect(() => {
+        let isMounted = true;
+        getCart().then((cartArray) => {
+            if (isMounted) setCart(cartArray);
+        });
+        return () => {
+            isMounted = false;
+        };
+    }, []);
+
+  return (
+    <div className='cart-page'>
+        <h1 className='text-center'>Your Cart</h1>
+        <div className='cart-container'>
+            {cart.map((item) => (
+                <CartCard key={item.key} cart={cart} setCart={setCart} />
+            ))}
+        </div>
+    </div>
+  )
+}
